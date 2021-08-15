71b6bd64dad6671404702b8b4a38d5b9fc9ab1d1
Tyler Johnson <tyler@csdconsultants.us>
2021-08-14 19:30:41 -0700

    installs expo camera, adds camera functionality and styling

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList, Linking } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList, Linking, TouchableOpacity } from 'react-native';
 import * as Contacts from 'expo-contacts';
-import { NativeBaseProvider } from 'native-base';
+import { Camera } from 'expo-camera';
 
 export default function App() {
 
   const [contacts, setContacts] = useState([]);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     const getContacts = async () => {
       const { status } = await Contacts.requestPermissionsAsync();
-      if(status === 'granted') {
+      if (status === 'granted') {
         let contactList = await Contacts.getContactsAsync();
         setContacts(contactList.data);
       }
@@ -25,24 +27,52 @@ export default function App() {
     const link = `tel:${phoneNumber}`;
 
     Linking.canOpenURL(link)
-    .then(supported => Linking.openURL(link))
-    .catch(console.error);
+      .then(supported => Linking.openURL(link))
+      .catch(console.error);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
 
   return (
-    <NativeBaseProvider>
-        <View style={styles.container}>
-        <Text style={styles.text}>About time this worked</Text>
-        <StatusBar style="auto" />
-        <Button onPress={() => console.log('button pressed')} title="Click Here"/>
-        <FlatList
-          style={styles.text}
-          data={contacts}
-          renderItem={({ item }) => item.name === undefined ? <Button title='no contact' onPress={() => console.log(item.name)} /> : <Button title={item.name} onPress={() => call(item)}/>}
-          keyExtractor={item => item.id}
-        />
-      </View>
-    </NativeBaseProvider>
+    <View style={styles.container}>
+      <Text style={styles.text}>This app looks terrible!</Text>
+        <Camera style={styles.camera} type={type}>
+          <View style={styles.view}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}>
+              <Text style={styles.button}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      <StatusBar style="auto" />
+      <Button onPress={() => console.log('button pressed')} title="Click Here" />
+      <FlatList
+        style={styles.text}
+        data={contacts}
+        renderItem={({ item }) => item.name === undefined ? <Button title='no contact' onPress={() => console.log(item.name)} /> : <Button title={item.name} onPress={() => call(item)} />}
+        keyExtractor={item => item.id}
+      />
+    </View>
   );
 }
 
@@ -56,13 +86,24 @@ const styles = StyleSheet.create({
   text: {
     flex: 1,
     fontSize: 50,
-    borderColor:'#0e0',
+    borderColor: '#0e0',
     borderWidth: 2,
     marginTop: 45,
+    padding: 5,
   },
   list: {
     flex: 2,
     borderColor: 'red',
     borderWidth: 2,
-  }
+  },
+  camera: {
+    height: '40%',
+    width: '100%',
+  },
+  button: {
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+    height: 40,
+    width: 40,
+  },
 });
